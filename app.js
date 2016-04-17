0940d139451d60bbcb0cc649c9bf92992993ef43
Arin <graffymint@gmail.com>
2016-04-17 18:12:38 +0300

    Edited the Register page

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(validate());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static((__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
